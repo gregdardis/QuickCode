498a7bc8b13dac1dcd1bf0b2f2809f65d4d5122e
Greg Dardis <dardis.greg@gmail.com>
2017-10-31 15:59:30 -0600

    made basic structure of site, got jquery selectable working

--- js/quick-code.js
+++ js/quick-code.js
@@ -1,1 +1,8 @@
-$("#draggable").draggable();
+$(document).ready(function() {
+    // selects HTML option in selectable upon document start
+    $("#html-li").addClass("ui-selected");
+})
+
+$(function() {
+     $("#language-selector").selectable();
+});
